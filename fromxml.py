7550d11ce57eea870b4c15cb1d72539036b1b3fa
Tanel Treuberg <60236942+The-Magicians-Code@users.noreply.github.com>
2022-09-13 16:33:09 +0300

    XSD schema parsing in pythonh

--- fromxml.py
+++ fromxml.py
@@ -1,12 +1,10 @@
-import xml.etree.ElementTree as ET
+from lxml import etree
 
-root = ET.parse("settings.xml")
-    
-for item in root.findall("app"):
-    print(item.get("name"))
-    for t in item:
-        print(t.tag, t.text)
-    print()
-    
-# tags = {elem.tag for elem in root.iter()}
-# print(tags)
+# https://xmlschema.readthedocs.io/en/latest/usage.html#data-decoding-and-encoding
+# https://www.freeformatter.com/xsd-generator.html#before-output
+
+xml_schema_doc = etree.parse("schema.xml")
+xml_doc = etree.parse("settings.xml")
+xml_schema = etree.XMLSchema(xml_schema_doc)
+status = xml_schema.validate(xml_doc)
+print(xml_schema.error_log, status)
